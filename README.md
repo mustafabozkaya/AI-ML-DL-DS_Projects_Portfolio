e65f775de3482ebfc04e3286e7b5e24d4864ca6d
mustafabozkaya <mustafaegebozkaya555@hotmail.com>
2022-12-18 23:21:16 +0300

    Added folder structure for machine learning projects

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # AI-ML-DL-DS_Projects_Portfolio
+
 "Welcome to my portfolio of artificial intelligence, machine learning, deep learning, and data science projects! This repository contains a collection of projects that showcase my skills and experience in these areas.
+
+
+
+AI-ML-DL-DS_Projects_Portfolio
+
+* Project 1: Predicting Stock Prices using Financial Data
+
+  * Data
+  * Code
+  * Presentation
+* Project 2: Deep Learning for Image Classification
+
+  * Data
+  * Code
+  * Presentation
+* Project 3: Social Media Sentiment Analysis
+
+  * Data
+  * Code
+  * Presentation
+* Project 4: Fraud Detection in Financial Transactions
+
+  * Data
+  * Code
+  * Presentation
+* Project 5: Personalized Recommendation Engine for E-Commerce Websites
+
+  * Data
+  * Code
+  * Presentation
+* Project 6: Natural Language Processing for Language Translation
+
+  * Data
+  * Code
+  * Presentation
+* Project 7: Predictive Maintenance in Manufacturing
+
+  * Data
+  * Code
+  * Presentation
+* Project 8: Predictive Modeling for Healthcare Outcomes
+
+  * Data
+  * Code
+  * Presentation
+* Project 9: Traffic Flow Prediction Using Transportation Data
+
+  * Data
+  * Code
+  * Presentation
+* Project 10: Text Generation with Machine Learning
+
+  * Data
+  * Code
+  * Presentation
+* Project 11: Voice Recognition and Speech Synthesis
+
+  * Data
+  * Code
+  * Presentation
+* Project 12: Predictive Modeling for Energy Consumption
+
+  * Data
+  * Code
+  * Presentation
+* Project 13: Predictive Modeling for Weather Forecasting
+
+  * Data
+  * Code
+  * Presentation
+* Project 14: Time Series Analysis for Sales Forecasting
+
+  * Data
+  * Code
+  * Presentation
+* Project 15: Customer Segmentation Using Clustering Algorithms
+
+  * Data
+  * Code
+  * Presentation
---
+++ create_projects.sh
@@ -0,0 +1,117 @@
+# change folder home directory
+cd
+# Navigate to the AI-ML-DL-DS_Projects_Portfolio repository
+cd AI-ML-DL-DS_Projects_Portfolio
+
+# Create the subfolders for each project
+mkdir "Project 1: Predicting Stock Prices using Financial Data"
+mkdir "Project 2: Deep Learning for Image Classification"
+mkdir "Project 3: Social Media Sentiment Analysis"
+mkdir "Project 4: Fraud Detection in Financial Transactions"
+mkdir "Project 5: Personalized Recommendation Engine for E-Commerce Websites"
+mkdir "Project 6: Natural Language Processing for Language Translation"
+mkdir "Project 7: Predictive Maintenance in Manufacturing"
+mkdir "Project 8: Predictive Modeling for Healthcare Outcomes"
+mkdir "Project 9: Traffic Flow Prediction Using Transportation Data"
+mkdir "Project 10: Text Generation with Machine Learning"
+mkdir "Project 11: Voice Recognition and Speech Synthesis"
+mkdir "Project 12: Predictive Modeling for Energy Consumption"
+mkdir "Project 13: Predictive Modeling for Weather Forecasting"
+mkdir "Project 14: Time Series Analysis for Sales Forecasting"
+mkdir "Project 15: Customer Segmentation Using Clustering Algorithms"
+
+# Navigate into each project folder
+cd "Project 1: Predicting Stock Prices using Financial Data"
+
+# Create the subfolders for each project
+mkdir Data
+mkdir Code
+mkdir Presentation
+
+# Navigate back to the main folder
+cd ..
+
+# Repeat the process for each project folder
+cd "Project 2: Deep Learning for Image Classification"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 3: Social Media Sentiment Analysis"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 4: Fraud Detection in Financial Transactions"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 5: Personalized Recommendation Engine for E-Commerce Websites"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 6: Natural Language Processing for Language Translation"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 7: Predictive Maintenance in Manufacturing"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 8: Predictive Modeling for Healthcare Outcomes"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 9: Traffic Flow Prediction Using Transportation Data"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 10: Text Generation with Machine Learning"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 11: Voice Recognition and Speech Synthesis"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 12: Predictive Modeling for Energy Consumption"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 13: Predictive Modeling for Weather Forecasting"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 14: Time Series Analysis for Sales Forecasting"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
+
+cd "Project 15: Customer Segmentation Using Clustering Algorithms"
+mkdir Data
+mkdir Code
+mkdir Presentation
+cd ..
